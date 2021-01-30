b75f74832c0187150e1fa3dff60276c4769b961d
kensuke suzuki <35k.0101@gmail.com>
2021-01-30 20:45:52 +0900

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+    name: 'A山B郎',
+    course: 'Webエンジニアコース',
+    acceptancePeriod: '2019年01月期',
+    defaultLastId: 3,
+    students: [
+      { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
+      { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
+      { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
+    ]
+  },
+  methods: {
+    addStudent: function () {
+
+    }
+  }
+})
