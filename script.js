23d1264cc5e95a44bf9ce91106be8c3f6b52dc2c
kensuke suzuki <35k.0101@gmail.com>
2021-01-30 20:57:58 +0900

    アカウント追加機能を実装

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'A山B郎',
-    course: 'Webエンジニアコース',
-    acceptancePeriod: '2019年01月期',
-    defaultLastId: 3,
+    name: '',
+    course: '',
+    acceptancePeriod: '',
+    defaultLastId: 4,
     students: [
       { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
       { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
@@ -13,7 +13,15 @@ new Vue({
   },
   methods: {
     addStudent: function () {
-
+      this.students.push({
+        id: this.defaultLastId++,
+        name: this.name,
+        course: this.course,
+        acceptancePeriod: this.acceptancePeriod
+      })
+      this.name = "",
+        this.course = "",
+        this.acceptancePeriod = ""
     }
   }
 })
